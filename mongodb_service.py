b28c503226e434b07e8a266b5cebd00381afb3be
gribanov_dmitrii <gds1984@bk.ru>
2021-08-11 20:03:45 +0500

    base created, articles into base

---
+++ mongodb_service.py
@@ -0,0 +1,50 @@
+import typing
+from pymongo import MongoClient
+from pymongo.collection import Collection
+
+
+class MyMongoClient:
+    def __init__(self):
+        self._client = MongoClient('localhost')
+        self._db = self._client['ml_base']
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def database(self):
+        return self._db
+
+    def create_collection(self, name: str) -> Collection:
+        """
+        Создать объект коллекции
+        """
+        return self.database.create_collection(name)
+
+    def get_collection(self, name: str) -> Collection:
+        """
+        Получить объект коллекции
+        """
+        return self.database.get_collection(name)
+
+    def save_and_update_database(self, collection: Collection, dataset: typing.List):
+        """
+        Проверить список данных на наличие в БД, если их нет, то сохранить в БД
+        """
+        new_objects = []
+        for data in dataset:
+            filters = {'$and': [{"name": data.get('name')},
+                                {"date_publication": data.get('date_publication')}]}
+            obj = self.get_documents_collection(collection, filters)
+            if not list(obj):
+                new_objects.append(data)
+        if new_objects:
+            collection.insert_many(new_objects)
+            [print(f'Записано {_}') for _ in new_objects]
+
+    def get_documents_collection(self, collection: Collection, filters: dict = {}):
+        """
+        Получить список документов конкретной коллекции, соответствующий фильтру
+        """
+        return list(collection.find(filters))
--- parse_urls.py
+++ parse_urls.py
@@ -3,6 +3,7 @@ import re
 import requests
 import time
 from bs4 import BeautifulSoup as bs
+from pymongo import MongoClient
 
 
 def get_response(url):
@@ -23,21 +24,31 @@ file_name = 'digest_records_from_2021-7-16_6-17-58_UTC.json'
 address_set = set()
 without_article_tag_count = 0
 
+client = MongoClient('localhost')
+db = client['ml_base']
+ml_collection = db.ml_collection
+
 with open(file_name, 'r') as file:
     file = json.load(file)
+
     for itm in file:
-        address = re.findall(r'\/{2}([^\/]*)\/', itm['fields']['url'])
-        address_set.add('.'.join(address[0].split('.')[-2:])) if address else address_set.add('None')
+        if ml_collection.count_documents({'pk': itm['pk']}) == 0:
+            response = get_response(itm['fields']['url'])
+            soup = bs(response.text, 'lxml')
+            article_soup = soup.find('article')
 
-        response = get_response(itm['fields']['url'])
-        soup = bs(response.text, 'lxml')
-        article_soup = soup.find('article')
-        if article_soup:
-            for para in article_soup.find_all("p"):
-                print(para.get_text())
+            article_content = ''
+            if article_soup:
+                for paragraph in article_soup.find_all("p"):
+                    article_content = article_content + paragraph.get_text()
+            else:
+                article_content = 'No article tag'
+
+            itm['fields']['article_content'] = article_content
+
+            ml_collection.insert_one(itm)
         else:
-            without_article_tag_count += 1
-        print(1)
+            continue
 
-    # print(address_set, len(address_set))
-
+        # address = re.findall(r'\/{2}([^\/]*)\/', itm['fields']['url'])
+        # address_set.add('.'.join(address[0].split('.')[-2:])) if address else address_set.add('None')
